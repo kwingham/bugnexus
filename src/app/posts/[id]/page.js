b83105a19ba74391d6dabccb451fc6ce17a81440
Jonathan Taylor <jon.1807@hotmail.co.uk>
2024-10-30 08:50:01 +0000

    fixed await params error added on line 9

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -3,33 +3,33 @@ import CommentForm from "@/components/CommentForm";
 import DeletePostButton from "@/components/DeletePost";
 import EditPostButton from "@/components/EditPost";
 
-export default async function SingularPostPage({params}) {
-    
-    const db = connect();
+export default async function SingularPostPage({ params }) {
+  const db = connect();
 
-    const req = await db.query(`SELECT * FROM posts WHERE id = $1`, [params.id])
-    const post = await req.rows[0]
+  const { id } = await params;
 
+  const req = await db.query(`SELECT * FROM posts WHERE id = $1`, [id]);
+  const post = req.rows[0];
 
-    return (
+  return (
+    <div>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+      <div>
+        <CommentForm />
+      </div>
+      {post.clerk_id && (
         <div>
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
-            <div>
-            <CommentForm />
-            </div>
-            {post.clerk_id && (
-                <div>
-                  <DeletePostButton postId={post.id} />
-                </div>
-              )}
-              <div>
-                <EditPostButton
-                  postId={post.id}
-                  initialContent={post.body}
-                  className="text-black"
-                />
-              </div>
+          <DeletePostButton postId={post.id} />
         </div>
-    )
+      )}
+      <div>
+        <EditPostButton
+          postId={post.id}
+          initialContent={post.body}
+          className="text-black"
+        />
+      </div>
+    </div>
+  );
 }
