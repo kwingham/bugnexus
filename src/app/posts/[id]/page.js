91995a21319f1e2edc942ac04fd96eab1febf4cd
Shaun <shaunmadziva@gmail.com>
2024-10-30 10:24:36 +0000

    fixing-postPage-indention-error

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -5,9 +5,8 @@ import EditPostButton from "@/components/EditPost";
 import { CommentsList } from "@/components/CommentsList";
 
 export default async function SingularPostPage({ params }) {
-
   const { id } = await params;
-  
+
   const db = connect();
   const req = await db.query(`SELECT * FROM posts WHERE id = $1`, [id]);
   const post = req.rows[0];
@@ -21,26 +20,25 @@ export default async function SingularPostPage({ params }) {
       </div>
       {post.clerk_id && (
         <div>
-        comments-list
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
-            <div>
-            <CommentForm postId={post.id}/>
-            <CommentsList postId={post.id}/>
-            </div>
+          comments-list
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+          <div>
+            <CommentForm postId={post.id} />
+            <CommentsList postId={post.id} />
+          </div>
+          <div>
+            <EditPostButton
+              postId={post.id}
+              initialContent={post.body}
+              className="text-black"
+            />
+            {post.clerk_id && (
               <div>
-                <EditPostButton
-                  postId={post.id}
-                  initialContent={post.body}
-                  className="text-black"
-                />
-                 {post.clerk_id && (
-                <div>
-                  <DeletePostButton postId={post.id} />
-                </div>
-              )}
-              </div
-          <DeletePostButton postId={post.id} />
+                <DeletePostButton postId={post.id} />
+              </div>
+            )}
+          </div>
         </div>
       )}
     </div>
