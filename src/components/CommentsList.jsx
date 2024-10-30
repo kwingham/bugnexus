48fb1dc0965cbcab4d5d085f0d363d37f2655a89
Jonathan Taylor <jon.1807@hotmail.co.uk>
2024-10-30 15:49:05 +0000

    added conditional rendering

--- src/components/CommentsList.jsx
+++ src/components/CommentsList.jsx
@@ -1,7 +1,11 @@
 import { connect } from "@/utilities/db";
 import CommentForm from "./CommentForm";
 
-export async function CommentsList({ postId, parentCommentId = null }) {
+export async function CommentsList({
+  postId,
+  parentCommentId = null,
+  level = 0,
+}) {
   const db = connect();
   const commentQuery = `SELECT comments.id, comments.body, clerk_users.username 
      FROM comments 
@@ -17,21 +21,29 @@ export async function CommentsList({ postId, parentCommentId = null }) {
 
   const comments = await db.query(commentQuery, commentsArgs);
 
+  const borderColor = level % 2 === 0 ? "border-green-300" : "border-blue-300";
+
   return (
-    <ul className="space-y-4">
+    <ul className="space-y-4 max-w-full">
       {comments.rows.map((comment) => (
         <li
           key={comment.id}
-          className="bg-gray-800 p-4 rounded-lg shadow-md border border-green-300"
+          className={`bg-gray-800 p-4 rounded-lg shadow-md border ${borderColor}`}
         >
           <div className="mb-2">
-            <span className="text-green-400 font-semibold">{comment.name}</span>
+            <span className="text-green-400 font-semibold">
+              {comment.username}
+            </span>
           </div>
           <div className="text-gray-300">
             <p className="mb-4">{comment.body}</p>
             <CommentForm postId={postId} parentCommentId={comment.id} />
-            <div className="ml-6 mt-4">
-              <CommentsList postId={postId} parentCommentId={comment.id} />
+            <div className="pl-4 md:pl-6 mt-4 max-w-full">
+              <CommentsList
+                postId={postId}
+                parentCommentId={comment.id}
+                level={level + 1}
+              />
             </div>
           </div>
         </li>
